fd665ef7e794112d99400b1b3d7e6f4a762f95ec
Aldemas-py <njengasam2406@gmail.com>
2021-10-18 17:44:58 +0300

    started on ajax

---
+++ ajax/foodstore.php
@@ -0,0 +1,23 @@
+<?php
+header('Content-Type: text/xml');
+echo '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>';
+echo '<response>';
+
+    $food = $_GET['food'];
+    $foodArray = array('tuna', 'beef', 'loaf', 'bacon');
+
+    if (in_array($food, $foodArray)) {
+        echo 'We do have '.$food.'!';        # code...
+    }
+    elseif ($food=='') {
+        echo'Enter a food you Punk';
+        # code...
+    }
+    else{
+        echo 'Sorry punk we dont sell no '. $food . 'here! ';
+
+    }
+
+
+echo '</response>';
+?>
